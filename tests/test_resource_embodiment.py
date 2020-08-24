3cb716c4369ba2967ca27dbfe3374b7c89399aa9
Simone Persiani <iosonopersia@gmail.com>
2020-08-24 09:21:59 +0200

    Update test_resource_embodiment.py

--- tests/test_resource_embodiment.py
+++ tests/test_resource_embodiment.py
@@ -15,9 +15,66 @@
 # SOFTWARE.
 import unittest
 
+from rdflib import URIRef, Literal, RDF
+
+from oc_graphlib.graph_entity import GraphEntity
+from oc_graphlib.graph_set import GraphSet
+
 
 class TestResourceEmbodiment(unittest.TestCase):
-    pass
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.graph_set = GraphSet("http://test/", "context_base", "./info_dir/info_file_", 0, "", wanted_label=False)
+
+    def setUp(self):
+        self.graph_set.g = []
+        self.re = self.graph_set.add_re(self.__class__.__name__)
+
+    def test_has_media_type(self):
+        media_type = "MediaType"
+        result = self.re.has_media_type(media_type)
+        self.assertTrue(result)
+
+        triple = URIRef(str(self.re)), GraphEntity.has_format, Literal(media_type)
+        self.assertIn(triple, self.re.g)
+
+    def test_create_starting_page(self):
+        starting_page = "15"
+        result = self.re.create_starting_page(starting_page)
+        self.assertTrue(result)
+
+        triple = URIRef(str(self.re)), GraphEntity.starting_page, Literal(starting_page)
+        self.assertIn(triple, self.re.g)
+
+    def test_create_ending_page(self):
+        ending_page = "288"
+        result = self.re.create_ending_page(ending_page)
+        self.assertTrue(result)
+
+        triple = URIRef(str(self.re)), GraphEntity.ending_page, Literal(ending_page)
+        self.assertIn(triple, self.re.g)
+
+    def test_has_url(self):
+        url = "http://test/url"
+        result = self.re.has_url(url)
+        self.assertTrue(result)
+
+        triple = URIRef(str(self.re)), GraphEntity.has_url, Literal(url)
+        self.assertIn(triple, self.re.g)
+
+    def test_create_digital_embodiment(self):
+        result = self.re.create_digital_embodiment()
+        self.assertIsNone(result)
+
+        triple = URIRef(str(self.re)), RDF.type, GraphEntity.digital_manifestation
+        self.assertIn(triple, self.re.g)
+
+    def test_create_print_embodiment(self):
+        result = self.re.create_print_embodiment()
+        self.assertIsNone(result)
+
+        triple = URIRef(str(self.re)), RDF.type, GraphEntity.print_object
+        self.assertIn(triple, self.re.g)
 
 
 if __name__ == '__main__':
