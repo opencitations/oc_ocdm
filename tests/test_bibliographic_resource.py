e6fbcc0c40aebc2c1d0ad61a5a01c7faef944991
Simone Persiani <iosonopersia@gmail.com>
2020-08-24 07:51:08 +0200

    Update test_bibliographic_resource.py

--- tests/test_bibliographic_resource.py
+++ tests/test_bibliographic_resource.py
@@ -15,9 +15,400 @@
 # SOFTWARE.
 import unittest
 
+from rdflib import URIRef, Literal, XSD, RDF
+
+from oc_graphlib.graph_entity import GraphEntity
+from oc_graphlib.graph_set import GraphSet
+
 
 class TestBibliographicResource(unittest.TestCase):
-    pass
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.graph_set = GraphSet("http://test/", "context_base", "./info_dir/info_file_", 0, "", wanted_label=False)
+
+    def setUp(self):
+        self.graph_set.g = []
+        self.br1 = self.graph_set.add_br(self.__class__.__name__)
+        self.br2 = self.graph_set.add_br(self.__class__.__name__)
+        self.re = self.graph_set.add_re(self.__class__.__name__)
+        self.be = self.graph_set.add_be(self.__class__.__name__)
+        self.de = self.graph_set.add_de(self.__class__.__name__)
+
+    def test_create_title(self):
+        title = "Resource"
+        result = self.br1.create_title(title)
+        self.assertTrue(result)
+
+        triple = URIRef(str(self.br1)), GraphEntity.title, Literal(title)
+        self.assertIn(triple, self.br1.g)
+
+    def test_create_subtitle(self):
+        subtitle = "Resource"
+        result = self.br1.create_subtitle(subtitle)
+        self.assertTrue(result)
+
+        triple = URIRef(str(self.br1)), GraphEntity.has_subtitle, Literal(subtitle)
+        self.assertIn(triple, self.br1.g)
+
+    def test_has_part(self):
+        result = self.br1.has_part(self.br2)
+        self.assertIsNone(result)
+
+        triple = URIRef(str(self.br2)), GraphEntity.part_of, URIRef(str(self.br1))
+        self.assertIn(triple, self.br2.g)
+
+    def test_has_citation(self):
+        result = self.br1.has_citation(self.br2)
+        self.assertIsNone(result)
+
+        triple = URIRef(str(self.br1)), GraphEntity.cites, URIRef(str(self.br2))
+        self.assertIn(triple, self.br1.g)
+
+    def test_create_pub_date(self):
+        with self.subTest("date_list is [int, int, int]"):
+            string = "2020-05-25"
+            datatype = XSD.date
+            result = self.br1.create_pub_date([2020, 5, 25])
+            self.assertTrue(result)
+
+            triple = URIRef(str(self.br1)), GraphEntity.has_publication_date, Literal(string, datatype=datatype,
+                                                                                      normalize=False)
+            self.assertIn(triple, self.br1.g)
+        with self.subTest("date_list is [int, int]"):
+            string = "2020-05"
+            datatype = XSD.gYearMonth
+            result = self.br1.create_pub_date([2020, 5])
+            self.assertTrue(result)
+
+            triple = URIRef(str(self.br1)), GraphEntity.has_publication_date, Literal(string, datatype=datatype,
+                                                                                      normalize=False)
+            self.assertIn(triple, self.br1.g)
+        with self.subTest("date_list is [int]"):
+            string = "2020"
+            datatype = XSD.gYear
+            result = self.br1.create_pub_date([2020])
+            self.assertTrue(result)
+
+            triple = URIRef(str(self.br1)), GraphEntity.has_publication_date, Literal(string, datatype=datatype,
+                                                                                      normalize=False)
+            self.assertIn(triple, self.br1.g)
+        with self.subTest("date_list is [int, None]"):
+            string = "2020"
+            datatype = XSD.gYear
+            result = self.br1.create_pub_date([2020, None])
+            self.assertTrue(result)
+
+            triple = URIRef(str(self.br1)), GraphEntity.has_publication_date, Literal(string, datatype=datatype,
+                                                                                      normalize=False)
+            self.assertIn(triple, self.br1.g)
+        with self.subTest("date_list is [int, None, None]"):
+            string = "2020"
+            datatype = XSD.gYear
+            result = self.br1.create_pub_date([2020, None, None])
+            self.assertTrue(result)
+
+            triple = URIRef(str(self.br1)), GraphEntity.has_publication_date, Literal(string, datatype=datatype,
+                                                                                      normalize=False)
+            self.assertIn(triple, self.br1.g)
+        with self.subTest("date_list is [None, None, None]"):
+            prev_len = len(self.br1.g)
+            result = self.br1.create_pub_date([None, None, None])
+            self.assertFalse(result)
+            self.assertIsNotNone(result)
+
+            after_len = len(self.br1.g)
+            self.assertEqual(prev_len, after_len)
+        with self.subTest("date_list is empty"):
+            prev_len = len(self.br1.g)
+            result = self.br1.create_pub_date([])
+            self.assertFalse(result)
+            self.assertIsNotNone(result)
+
+            after_len = len(self.br1.g)
+            self.assertEqual(prev_len, after_len)
+        with self.subTest("date_list is None"):
+            prev_len = len(self.br1.g)
+            result = self.br1.create_pub_date()
+            self.assertFalse(result)
+            self.assertIsNotNone(result)
+
+            after_len = len(self.br1.g)
+            self.assertEqual(prev_len, after_len)
+        with self.subTest("date_list is [int, 1, int]"):
+            string = "2020-01-25"
+            datatype = XSD.date
+            result = self.br1.create_pub_date([2020, 1, 25])
+            self.assertTrue(result)
+
+            triple = URIRef(str(self.br1)), GraphEntity.has_publication_date, Literal(string, datatype=datatype,
+                                                                                      normalize=False)
+            self.assertIn(triple, self.br1.g)
+        with self.subTest("date_list is [int, 1, 1]"):
+            string = "2020"
+            datatype = XSD.gYear
+            result = self.br1.create_pub_date([2020, 1, 1])
+            self.assertTrue(result)
+
+            triple = URIRef(str(self.br1)), GraphEntity.has_publication_date, Literal(string, datatype=datatype,
+                                                                                      normalize=False)
+            self.assertIn(triple, self.br1.g)
+        with self.subTest("date_list is [int, 5, 1]"):
+            string = "2020-05-01"
+            datatype = XSD.date
+            result = self.br1.create_pub_date([2020, 5, 1])
+            self.assertTrue(result)
+
+            triple = URIRef(str(self.br1)), GraphEntity.has_publication_date, Literal(string, datatype=datatype,
+                                                                                      normalize=False)
+            self.assertIn(triple, self.br1.g)
+
+    def test_has_format(self):
+        result = self.br1.has_format(self.re)
+        self.assertIsNone(result)
+
+        triple = URIRef(str(self.br1)), GraphEntity.embodiment, URIRef(str(self.re))
+        self.assertIn(triple, self.br1.g)
+
+    def test_create_number(self):
+        number = "1234"
+        result = self.br1.create_number(number)
+        self.assertTrue(result)
+
+        triple = URIRef(str(self.br1)), GraphEntity.has_sequence_identifier, Literal(number)
+        self.assertIn(triple, self.br1.g)
+
+    def test_has_edition(self):
+        edition = "abcde"
+        result = self.br1.has_edition(edition)
+        self.assertTrue(result)
+
+        triple = URIRef(str(self.br1)), GraphEntity.has_edition, Literal(edition)
+        self.assertIn(triple, self.br1.g)
+
+    def test_contains_in_reference_list(self):
+        result = self.br1.contains_in_reference_list(self.be)
+        self.assertIsNone(result)
+
+        triple = URIRef(str(self.br1)), GraphEntity.contains_reference, URIRef(str(self.be))
+        self.assertIn(triple, self.br1.g)
+
+    def test_contains_discourse_element(self):
+        result = self.br1.contains_discourse_element(self.de)
+        self.assertIsNone(result)
+
+        triple = URIRef(str(self.br1)), GraphEntity.contains_de, URIRef(str(self.de))
+        self.assertIn(triple, self.br1.g)
+
+    def test_has_reference(self):
+        result = self.br1.has_reference(self.be)
+        self.assertIsNone(result)
+
+        triple = URIRef(str(self.be)), GraphEntity.references, URIRef(str(self.br1))
+        self.assertIn(triple, self.be.g)
+
+    def test_has_related_document(self):
+        document = URIRef("http://test/document")
+        result = self.br1.has_related_document(document)
+        self.assertIsNone(result)
+
+        triple = URIRef(str(self.br1)), GraphEntity.relation, document
+        self.assertIn(triple, self.br1.g)
+
+    def test_create_archival_document(self):
+        result = self.br1.create_archival_document()
+        self.assertIsNone(result)
+
+        triple = URIRef(str(self.br1)), RDF.type, GraphEntity.archival_document
+        self.assertIn(triple, self.br1.g)
+
+    def test_create_book(self):
+        result = self.br1.create_book()
+        self.assertIsNone(result)
+
+        triple = URIRef(str(self.br1)), RDF.type, GraphEntity.book
+        self.assertIn(triple, self.br1.g)
+
+    def test_create_book_chapter(self):
+        result = self.br1.create_book_chapter()
+        self.assertIsNone(result)
+
+        triple = URIRef(str(self.br1)), RDF.type, GraphEntity.book_chapter
+        self.assertIn(triple, self.br1.g)
+
+    def test_create_book_part(self):
+        result = self.br1.create_book_part()
+        self.assertIsNone(result)
+
+        triple = URIRef(str(self.br1)), RDF.type, GraphEntity.part
+        self.assertIn(triple, self.br1.g)
+
+    def test_create_book_section(self):
+        result = self.br1.create_book_section()
+        self.assertIsNone(result)
+
+        triple = URIRef(str(self.br1)), RDF.type, GraphEntity.expression_collection
+        self.assertIn(triple, self.br1.g)
+
+    def test_create_book_series(self):
+        result = self.br1.create_book_series()
+        self.assertIsNone(result)
+
+        triple = URIRef(str(self.br1)), RDF.type, GraphEntity.book_series
+        self.assertIn(triple, self.br1.g)
+
+    def test_create_book_set(self):
+        result = self.br1.create_book_set()
+        self.assertIsNone(result)
+
+        triple = URIRef(str(self.br1)), RDF.type, GraphEntity.book_set
+        self.assertIn(triple, self.br1.g)
+
+    def test_create_book_track(self):
+        result = self.br1.create_book_track()
+        self.assertIsNone(result)
+
+        triple = URIRef(str(self.br1)), RDF.type, GraphEntity.expression
+        self.assertIn(triple, self.br1.g)
+
+    def test_create_component(self):
+        result = self.br1.create_component()
+        self.assertIsNone(result)
+
+        triple = URIRef(str(self.br1)), RDF.type, GraphEntity.expression
+        self.assertIn(triple, self.br1.g)
+
+    def test_create_dataset(self):
+        result = self.br1.create_dataset()
+        self.assertIsNone(result)
+
+        triple = URIRef(str(self.br1)), RDF.type, GraphEntity.data_file
+        self.assertIn(triple, self.br1.g)
+
+    def test_create_dissertation(self):
+        result = self.br1.create_dissertation()
+        self.assertIsNone(result)
+
+        triple = URIRef(str(self.br1)), RDF.type, GraphEntity.thesis
+        self.assertIn(triple, self.br1.g)
+
+    def test_create_edited_book(self):
+        result = self.br1.create_edited_book()
+        self.assertIsNone(result)
+
+        triple = URIRef(str(self.br1)), RDF.type, GraphEntity.book
+        self.assertIn(triple, self.br1.g)
+
+    def test_create_journal_article(self):
+        result = self.br1.create_journal_article()
+        self.assertIsNone(result)
+
+        triple = URIRef(str(self.br1)), RDF.type, GraphEntity.journal_article
+        self.assertIn(triple, self.br1.g)
+
+    def test_create_issue(self):
+        result = self.br1.create_issue()
+        self.assertIsNone(result)
+
+        triple = URIRef(str(self.br1)), RDF.type, GraphEntity.journal_issue
+        self.assertIn(triple, self.br1.g)
+
+    def test_create_volume(self):
+        result = self.br1.create_volume()
+        self.assertIsNone(result)
+
+        triple = URIRef(str(self.br1)), RDF.type, GraphEntity.journal_volume
+        self.assertIn(triple, self.br1.g)
+
+    def test_create_journal(self):
+        result = self.br1.create_journal()
+        self.assertIsNone(result)
+
+        triple = URIRef(str(self.br1)), RDF.type, GraphEntity.journal
+        self.assertIn(triple, self.br1.g)
+
+    def test_create_monograph(self):
+        result = self.br1.create_monograph()
+        self.assertIsNone(result)
+
+        triple = URIRef(str(self.br1)), RDF.type, GraphEntity.book
+        self.assertIn(triple, self.br1.g)
+
+    def test_create_proceedings_article(self):
+        result = self.br1.create_proceedings_article()
+        self.assertIsNone(result)
+
+        triple = URIRef(str(self.br1)), RDF.type, GraphEntity.proceedings_paper
+        self.assertIn(triple, self.br1.g)
+
+    def test_create_proceedings(self):
+        result = self.br1.create_proceedings()
+        self.assertIsNone(result)
+
+        triple = URIRef(str(self.br1)), RDF.type, GraphEntity.academic_proceedings
+        self.assertIn(triple, self.br1.g)
+
+    def test_create_reference_book(self):
+        result = self.br1.create_reference_book()
+        self.assertIsNone(result)
+
+        triple = URIRef(str(self.br1)), RDF.type, GraphEntity.reference_book
+        self.assertIn(triple, self.br1.g)
+
+    def test_create_reference_entry(self):
+        result = self.br1.create_reference_entry()
+        self.assertIsNone(result)
+
+        triple = URIRef(str(self.br1)), RDF.type, GraphEntity.reference_entry
+        self.assertIn(triple, self.br1.g)
+
+    def test_create_report_series(self):
+        result = self.br1.create_report_series()
+        self.assertIsNone(result)
+
+        triple = URIRef(str(self.br1)), RDF.type, GraphEntity.series
+        self.assertIn(triple, self.br1.g)
+
+    def test_create_report(self):
+        result = self.br1.create_report()
+        self.assertIsNone(result)
+
+        triple = URIRef(str(self.br1)), RDF.type, GraphEntity.report_document
+        self.assertIn(triple, self.br1.g)
+
+    def test_create_standard_series(self):
+        result = self.br1.create_standard_series()
+        self.assertIsNone(result)
+
+        triple = URIRef(str(self.br1)), RDF.type, GraphEntity.series
+        self.assertIn(triple, self.br1.g)
+
+    def test_create_standard(self):
+        result = self.br1.create_standard()
+        self.assertIsNone(result)
+
+        triple = URIRef(str(self.br1)), RDF.type, GraphEntity.specification_document
+        self.assertIn(triple, self.br1.g)
+
+    def test_create_series(self):
+        result = self.br1.create_series()
+        self.assertIsNone(result)
+
+        triple = URIRef(str(self.br1)), RDF.type, GraphEntity.series
+        self.assertIn(triple, self.br1.g)
+
+    def test_create_expression_collection(self):
+        result = self.br1.create_expression_collection()
+        self.assertIsNone(result)
+
+        triple = URIRef(str(self.br1)), RDF.type, GraphEntity.expression_collection
+        self.assertIn(triple, self.br1.g)
+
+    def test_create_other(self):
+        result = self.br1.create_other()
+        self.assertIsNone(result)
+
+        triple = URIRef(str(self.br1)), RDF.type, GraphEntity.expression
+        self.assertIn(triple, self.br1.g)
 
 
 if __name__ == '__main__':
