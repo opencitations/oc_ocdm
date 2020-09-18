456f0d5820df39c397ec9fd9826178005c9d4afa
Simone Persiani <iosonopersia@gmail.com>
2020-09-18 21:10:08 +0200

    Update test_prov_set.py

--- tests/test_prov_set.py
+++ tests/test_prov_set.py
@@ -13,11 +13,151 @@
 # DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS
 # ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS
 # SOFTWARE.
+import os
 import unittest
+
+from oc_graphlib.WIP.resfinder import ResourceFinder
+from oc_graphlib.graph_set import GraphSet
+from oc_graphlib.prov_set import ProvSet
 
 
 class TestProvSet(unittest.TestCase):
-    pass
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.prov_subj_graph_set = GraphSet("http://test/", "context_base", "./info_dir/info_file_", 0, "",
+                                           wanted_label=False)
+
+        cls.rf = ResourceFinder(base_dir="./ccc/", base_iri="https://w3id.org/oc/ccc/",
+                                tmp_dir="tmp/",
+                                context_map={
+                                    "https://w3id.org/oc/ccc/context.json": "./context.json"
+                                },
+                                dir_split=10000,
+                                n_file_item=1000,
+                                default_dir="/")
+
+        cls.prov_set = ProvSet(prov_subj_graph_set=cls.prov_subj_graph_set, base_iri="http://test/",
+                               context_path="context_base", info_dir="./info_dir/info_file_",
+                               wanted_label=False, dir_split=10000, n_file_item=1000, supplier_prefix="070",
+                               triplestore_url="http://localhost:9999/blazegraph/sparql", default_dir="/",
+                               resource_finder=cls.rf)
+
+        cls.file_path = './info_dir/test_file.txt'
+        if not os.path.exists(os.path.dirname(cls.file_path)):
+            os.makedirs(os.path.dirname(cls.file_path))
+
+        if not os.path.isfile(cls.file_path):
+            with open(cls.file_path, "wb") as file:
+                first_line = ProvSet.trailing_char * (ProvSet.initial_line_len - 1) + '\n'
+                file.write(first_line.encode('ascii'))
+
+    def setUp(self):
+        self.prov_set.g = []
+        self.prov_subj_graph_set.g = []
+
+        # Reset test file content:
+        with open(self.file_path, "wb") as file:
+            first_line = ProvSet.trailing_char * (ProvSet.initial_line_len - 1) + '\n'
+            file.write(first_line.encode('ascii'))
+
+    def test_get_line_len(self):
+        with open(self.file_path, 'rb') as test_file:
+            line_len = ProvSet._get_line_len(test_file)
+            self.assertIsNotNone(line_len)
+            self.assertEqual(line_len, ProvSet.initial_line_len)
+            self.assertEqual(test_file.tell(), 0)
+
+    def test_increase_line_len(self):
+        increment = 1
+        result = ProvSet._increase_line_len(self.file_path, ProvSet.initial_line_len + increment)
+        self.assertIsNone(result)
+        with open(self.file_path, 'rt', encoding='ascii') as test_file:
+            for line in test_file:
+                self.assertEqual(len(line), ProvSet.initial_line_len + increment)
+
+        self.assertRaises(ValueError, ProvSet._increase_line_len, self.file_path, -1)
+        self.assertRaises(ValueError, ProvSet._increase_line_len, self.file_path, ProvSet.initial_line_len - 1)
+
+    def test_is_a_valid_line(self):
+        with self.subTest("line is 'abc \\n'"):
+            line = 'abc \n'.encode('ascii')
+            result = ProvSet._is_a_valid_line(line)
+            self.assertIsNotNone(result)
+            self.assertTrue(result)
+        with self.subTest("line is 'a\\0c \\n'"):
+            line = 'a\0c \n'.encode('ascii')
+            result = ProvSet._is_a_valid_line(line)
+            self.assertIsNotNone(result)
+            self.assertFalse(result)
+        with self.subTest("line is 'abc'"):
+            line = 'abc'.encode('ascii')
+            result = ProvSet._is_a_valid_line(line)
+            self.assertIsNotNone(result)
+            self.assertFalse(result)
+        with self.subTest("line is 'a\\0c'"):
+            line = 'a\\0c'.encode('ascii')
+            result = ProvSet._is_a_valid_line(line)
+            self.assertIsNotNone(result)
+            self.assertFalse(result)
+
+    def test_fix_previous_lines(self):
+        with open(self.file_path, "wb") as test_file:
+            num_lines = 10
+            for i in range(0, num_lines):
+                line = '\0' * ProvSet.initial_line_len
+                test_file.write(line.encode('ascii'))
+            last_line = '1'.ljust(ProvSet.initial_line_len - 1, ProvSet.trailing_char) + '\n'
+            test_file.write(last_line.encode('ascii'))
+
+        with open(self.file_path, 'r+b') as test_file:
+            test_file.seek(ProvSet.initial_line_len * num_lines)
+            result = ProvSet._fix_previous_lines(test_file, ProvSet.initial_line_len)
+            self.assertIsNone(result)
+
+        with open(self.file_path, 'rt', encoding='ascii') as test_file:
+            count = 0
+            for line in test_file:
+                count += 1
+                if count >= num_lines:
+                    break
+                self.assertTrue(ProvSet._is_a_valid_line(line.encode('ascii')))
+
+    def test_read_number(self):
+        number = 18
+        with open(self.file_path, 'r+b') as test_file:
+            num_of_line = 35
+            test_file.seek(ProvSet.initial_line_len * (num_of_line - 1))
+            line = str(number).ljust(ProvSet.initial_line_len - 1, ProvSet.trailing_char) + '\n'
+            test_file.write(line.encode('ascii'))
+
+        read_number, line_len = ProvSet._read_number(self.file_path, num_of_line)
+        self.assertIsNotNone(read_number)
+        self.assertIsNotNone(line_len)
+        self.assertEqual(read_number, number)
+        self.assertEqual(line_len, ProvSet.initial_line_len)
+
+        self.assertRaises(ValueError, ProvSet._read_number, self.file_path, -1)
+
+    def test_add_number(self):
+        number = 18
+        with open(self.file_path, 'r+b') as test_file:
+            num_of_line = 35
+            test_file.seek(ProvSet.initial_line_len * (num_of_line - 1))
+            line = str(number).ljust(ProvSet.initial_line_len - 1, ProvSet.trailing_char) + '\n'
+            test_file.write(line.encode('ascii'))
+
+        read_number = ProvSet._add_number(self.file_path, num_of_line)
+        self.assertIsNotNone(read_number)
+        self.assertEqual(read_number, number + 1)
+        with open(self.file_path, 'rt', encoding='ascii') as test_file:
+            count = 0
+            for line in test_file:
+                count += 1
+                if count >= num_of_line:
+                    break
+                self.assertTrue(ProvSet._is_a_valid_line(line.encode('ascii')))
+
+        self.assertRaises(ValueError, ProvSet._add_number, self.file_path, -1)
 
 
 if __name__ == '__main__':
