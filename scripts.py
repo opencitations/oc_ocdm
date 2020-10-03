1d8e9ca3270b8cdb83827bdca5699d2d0733e951
Simone Persiani <iosonopersia@gmail.com>
2020-10-03 14:22:18 +0200

    Add new clean_info_dir script

--- scripts.py
+++ scripts.py
@@ -1,4 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Copyright (c) 2016, Silvio Peroni <essepuntato@gmail.com>
+#
+# Permission to use, copy, modify, and/or distribute this software for any purpose
+# with or without fee is hereby granted, provided that the above copyright notice
+# and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+# REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
+# FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT,
+# OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE,
+# DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS
+# ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS
+# SOFTWARE.
 import subprocess
+import os
+
 
 def test():
     """
@@ -8,3 +25,12 @@ def test():
     subprocess.run(
         ['python', '-u', '-m', 'unittest', 'discover']
     )
+
+
+def clean_info_dir():
+    """
+    Clean info_dir content
+    """
+    for file_path in os.listdir('./info_dir'):
+        os.remove(os.path.join('./info_dir', file_path))
+    print('Done! info_dir is now empty.')
