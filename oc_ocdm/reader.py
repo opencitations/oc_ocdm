f7a94bdd5bc3dcd034c5db5c93636a9b986c2a4e
Simone Persiani <iosonopersia@gmail.com>
2021-01-05 11:15:02 +0100

    Fix reader.py

--- oc_ocdm/reader.py
+++ oc_ocdm/reader.py
@@ -20,13 +20,13 @@ from pyshex import ShExEvaluator
 from rdflib import RDF, Namespace, ConjunctiveGraph
 from typing import TYPE_CHECKING
 
-from oc_ocdm.abstract_entity import AbstractEntity
+from oc_ocdm.graph import GraphEntity
 
 if TYPE_CHECKING:
     from typing import List, Set
-    from oc_ocdm.graph import GraphSet, GraphEntity
     from rdflib import Graph, URIRef, term
     from rdflib.query import Result
+    from oc_ocdm.graph import GraphSet
 
 
 def get_graph_from_subject(graph: Graph, subject: URIRef) -> Graph:
@@ -120,7 +120,7 @@ def graph_validation(graph: Graph, closed: bool = False) -> Graph:
 
 
 def import_entities_from_graph(g_set: GraphSet, graph: Graph, enable_validation: bool = True,
-                               closed: bool = False) -> List[AbstractEntity]:
+                               closed: bool = False) -> List[GraphEntity]:
     if enable_validation:
         graph = graph_validation(graph, closed)
 
@@ -179,15 +179,15 @@ def import_entities_from_graph(g_set: GraphSet, graph: Graph, enable_validation:
 
 
 def import_entity_from_triplestore(g_set: GraphSet, ts_url: str, res: URIRef,
-                                   enable_validation: bool = True) -> AbstractEntity:
+                                   enable_validation: bool = True) -> GraphEntity:
     ts: ConjunctiveGraph = ConjunctiveGraph()
     ts.open((ts_url, ts_url))
     query: str = f"CONSTRUCT {{<{res}> ?p ?o}} WHERE {{<{res}> ?p ?o}}"
 
     result: Result = ts.query(query)
     if result is not None:
-        imported_entities: List[AbstractEntity] = import_entities_from_graph(g_set, result.graph,
-                                                                          enable_validation=enable_validation)
+        imported_entities: List[GraphEntity] = import_entities_from_graph(g_set, result.graph,
+                                                                          enable_validation)
         ts.close()
         if len(imported_entities) <= 0:
             raise ValueError("The required entity was not found or was not recognized as a proper OCDM entity.")
