109f79b74947beeb19eaa1f0a8e630d769640659
Simone Persiani <iosonopersia@gmail.com>
2021-01-07 23:13:10 +0100

    Bump version number

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Silvio Peroni, Marilena Daquino, Fabio Mariani, Simone Persia
 author = 'Silvio Peroni, Marilena Daquino, Fabio Mariani, Simone Persiani'
 
 # The full version, including alpha/beta/rc tags
-release = '3.1.0'
+release = '4.0.0'
 
 
 # -- General configuration ---------------------------------------------------
