7197e910584ae755a8a3adaba8b8c1f26b8dd168
Simone Persiani <iosonopersia@gmail.com>
2021-04-17 12:30:13 +0200

    Add missing test for method remove_duplicated_identifiers

--- oc_ocdm/graph/entities/test/test_bibliographic_entity.py
+++ oc_ocdm/graph/entities/test/test_bibliographic_entity.py
@@ -15,6 +15,8 @@
 # SOFTWARE.
 import unittest
 
+from rdflib import Namespace
+
 from oc_ocdm.graph.graph_entity import GraphEntity
 from oc_ocdm.graph.graph_set import GraphSet
 
@@ -34,6 +36,39 @@ class TestBibliographicEntity(unittest.TestCase):
 
         triple = self.entity.res, GraphEntity.iri_has_identifier, self.identifier.res
         self.assertIn(triple, self.entity.g)
+
+    def test_remove_duplicated_identifiers(self):
+        id1 = self.graph_set.add_id(self.__class__.__name__)
+        id2 = self.graph_set.add_id(self.__class__.__name__)
+        id3 = self.graph_set.add_id(self.__class__.__name__)
+        id4 = self.graph_set.add_id(self.__class__.__name__)
+
+        id1.create_issn('1111-2222')
+        id2.create_doi('1111-2222')
+        id3.create_issn('3333-4444')
+        id4.create_issn('1111-2222')
+
+        self.entity.has_identifier(id1)
+        self.entity.has_identifier(id2)
+        self.entity.has_identifier(id3)
+        self.entity.has_identifier(id4)
+
+        result = self.entity.remove_duplicated_identifiers()
+
+        self.assertIsNone(result)
+
+        id_list = self.entity.get_identifiers()
+        self.assertEqual(3, len(id_list))
+
+        # Tuples were used down below inside the sets because they're are hashable (since they're immutable):
+        id_list_set = {(i.get_scheme(), i.get_literal_value()) for i in id_list}
+
+        datacite = Namespace("http://purl.org/spar/datacite/")
+        non_duplicated_ids = {(datacite.issn, '1111-2222'),
+                              (datacite.issn, '3333-4444'),
+                              (datacite.doi, '1111-2222')}
+
+        self.assertSetEqual(id_list_set, non_duplicated_ids)
 
 
 if __name__ == '__main__':
